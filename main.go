edce1c15940e6fcd10546219cd668da0fb818d5b
Hamza <hamza_zouhair@carrefour.com>
2021-03-18 22:39:55 +0100

    add makefile

--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"github.com/HamzaZo/kubectl-pod-viewer/cmd"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
-	_ "k8s.io/client-go/plugin/pkg/client/auth"  //required for auth
+	_ "k8s.io/client-go/plugin/pkg/client/auth" //required for auth
 	"os"
 )
 
-func main(){
+func main() {
 	root := cmd.NewCmdPodViewer(genericclioptions.IOStreams{
-		In: os.Stdin,
-		Out: os.Stdout,
+		In:     os.Stdin,
+		Out:    os.Stdout,
 		ErrOut: os.Stderr,
 	})
 	if err := root.Execute(); err != nil {
